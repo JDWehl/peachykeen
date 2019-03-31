18b9e912bc72336d9ff874259ca307f9d91c1f46
JDWehl <49086474+JDWehl@users.noreply.github.com>
2019-03-30 21:45:26 -0500

    Update Main.js
    Trying out some garbage

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
 
 class Main extends Component {
+	
+	
+	state = {
+		restaurants: []
+	}
+	var listHTML = '';
+	
+	fetch('/restaurants')
+		.then(res => res.json())
+		.then(restaurants => this.setState({ restaurants }));
+		.then(res.forEach(function(item) {
+			listHTML += '<li>' + item.name + '</li>';
+		}));
+		
+	document.getElementById("restaurants-list").innerHTML = '<ul>' + listHTML + '</ul>';
+	  
   render() {
     return (
       <div className="App">
-        Main
+		Main
       </div>
+	  <br>
+	  <div id="restaurants-list">
+	  </div>
     );
   }
 }
